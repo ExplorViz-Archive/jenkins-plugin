1bdcec2056ec4ff79fcb92be6a6e0b65fa5b0d55
Jan Erik Petersen <JanErikPetersen@web.de>
2019-07-29 02:42:35 +0200

    Kieker build step logs all records to files, auto-generated monitoring config

--- src/main/java/net/explorviz/jenkins/KiekerBuilder.java
+++ src/main/java/net/explorviz/jenkins/KiekerBuilder.java
@@ -1,16 +1,13 @@
 package net.explorviz.jenkins;
 
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Launcher;
-import hudson.model.AbstractProject;
-import hudson.model.Run;
-import hudson.model.TaskListener;
+import hudson.*;
+import hudson.model.*;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.ArgumentListBuilder;
 import hudson.util.FormValidation;
 import hudson.util.QuotedStringTokenizer;
+import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -19,17 +16,19 @@ import org.kohsuke.stapler.QueryParameter;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 @SuppressWarnings("unused")
 public class KiekerBuilder extends Builder implements SimpleBuildStep {
     private static final String ARG_JAVA_AGENT = "-javaagent:";
     private static final String ARG_KIEKER_MONITORING_CONFIGURATION = "-Dkieker.monitoring.configuration=";
     private static final String ARG_ASPECTJ_WEAVER_CONFIGURATION = "-Dorg.aspectj.weaver.loadtime.configuration=";
+    private static final String ARG_SKIP_DEFAULT_AOP_CONFIGURATION = "-Dkieker.monitoring.skipDefaultAOPConfiguration=true";
 
     private final String appJar;
     private String appArgs;
+    private boolean skipDefaultAOP;
     private final String aopXml;
     private String vmOpts;
     private String kiekerJar;
@@ -37,16 +36,14 @@ public class KiekerBuilder extends Builder implements SimpleBuildStep {
 
     @DataBoundConstructor
     public KiekerBuilder(String aopXml, String appJar) {
+        // Must match defaults in KiekerBuilder/config.jelly
         this.appJar = appJar;
         this.appArgs = "";
+        this.skipDefaultAOP = false;
         this.aopXml = aopXml;
         this.vmOpts = "";
         this.kiekerJar = "";
         this.failBuildOnEmpty = true;
-    }
-
-    public String getAopXml() {
-        return aopXml;
     }
 
     public String getAppJar() {
@@ -60,6 +57,19 @@ public class KiekerBuilder extends Builder implements SimpleBuildStep {
     @DataBoundSetter
     public void setAppArgs(String appArgs) {
         this.appArgs = appArgs;
+    }
+
+    public boolean isSkipDefaultAOP() {
+        return skipDefaultAOP;
+    }
+
+    @DataBoundSetter
+    public void setSkipDefaultAOP(boolean skipDefaultAOP) {
+        this.skipDefaultAOP = skipDefaultAOP;
+    }
+
+    public String getAopXml() {
+        return aopXml;
     }
 
     public String getVmOpts() {
@@ -92,28 +102,45 @@ public class KiekerBuilder extends Builder implements SimpleBuildStep {
     @Override
     public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher,
                         @Nonnull TaskListener listener) throws InterruptedException, IOException {
-        // TODO: Correct kieker path
-        String javaagent = kiekerJar.isEmpty() ? "kieker-1.14-SNAPSHOT-aspectj.jar" : kiekerJar;
+        FilePath workingDirectory = workspace.createTempDir("kieker", null);
 
-        // TODO: Automatically generate kieker monitoring configuration
-        String kiekerMonitoringConfiguration = "META-INF/kieker.monitoring.configuration";
+        String javaagent = Util.fixEmptyAndTrim(kiekerJar);
+        if (javaagent == null) {
+            // TODO: Copy/Deploy/Download bundled kieker jar to workingDirectory
+            javaagent = "kieker-1.14-SNAPSHOT-aspectj.jar";
+        }
 
-        // TODO: Find proper java executable based on Jenkins tool config
+        // Create kieker monitoring configuration
+        KiekerMonitoringConfiguration monitoringConfiguration = new KiekerMonitoringConfiguration();
+        monitoringConfiguration.setApplicationName(run.getParent().getFullDisplayName() + "_" + run.getId());
+        monitoringConfiguration.setOutputDirectory(workingDirectory.getRemote());
+
+        FilePath monitoringConfigurationFile = workingDirectory.child("kieker.monitoring.configuration");
+        monitoringConfiguration.write(monitoringConfigurationFile);
+
         ArgumentListBuilder argList = new ArgumentListBuilder();
-        argList.add("java");
+        // TODO: Make JDK selectable
+        argList.add(Jenkins.get().getJDKs().get(0).getBinDir() + "/java");
         argList.add(ARG_JAVA_AGENT + javaagent);
         argList.add(ARG_ASPECTJ_WEAVER_CONFIGURATION + aopXml);
-        argList.add(ARG_KIEKER_MONITORING_CONFIGURATION + kiekerMonitoringConfiguration);
+        argList.add(ARG_KIEKER_MONITORING_CONFIGURATION + monitoringConfigurationFile.getRemote());
+        if (skipDefaultAOP) {
+            argList.add(ARG_SKIP_DEFAULT_AOP_CONFIGURATION);
+        }
         argList.add(QuotedStringTokenizer.tokenize(vmOpts));
         argList.add("-jar", appJar);
-        if (!appArgs.isEmpty()) {
+        if (!appArgs.trim().isEmpty()) {
             argList.add("--");
             argList.add(QuotedStringTokenizer.tokenize(appArgs));
         }
 
-        launcher.launch().stdout(listener).pwd(workspace).cmds(argList).join();
+        Proc kiekerProc = launcher.launch().stdout(listener).pwd(workspace).cmds(argList).start();
+        if (kiekerProc.join() != 0) {
+            run.setResult(Result.FAILURE);
+        }
     }
 
+    @SuppressWarnings("MethodMayBeStatic")
     @Symbol("kieker")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
@@ -142,7 +169,7 @@ public class KiekerBuilder extends Builder implements SimpleBuildStep {
         }
 
         public FormValidation doCheckVmOpts(@QueryParameter String value) {
-            Set<FormValidation> warnings = new HashSet<>(0);
+            Collection<FormValidation> warnings = new HashSet<>(0);
 
             if (value.contains("-javaagent")) {
                 warnings.add(FormValidation.warning("Do not specify -javaagent! It is automatically set to kieker."));
@@ -156,7 +183,7 @@ public class KiekerBuilder extends Builder implements SimpleBuildStep {
             }
             if (value.contains("-Dorg.aspectj.weaver.loadtime.configuration")) {
                 warnings.add(FormValidation.warning("Do not specify the aspectj weaver configuration!" +
-                    "It is auto-generated and other configurations won't work."));
+                    "It is set automatically."));
             }
 
             return FormValidation.aggregate(warnings);
